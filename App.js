88f414b35cdeccf6f34e56b2c5f82515704b1dd9
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-01-04 10:45:25 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Ask from './pages/Ask';
+import Comments from './pages/Comments';
+import HackerNews from './pages/HackerNews';
+import Jobs from './pages/Jobs';
+import Login from './pages/Login';
+import New from './pages/New';
+import Past from './pages/Past';
+import Show from './pages/Show';
+import Submit from './pages/Submit';
+import Menu from './components/Menu';
+import './App.css';
+
+const App = () => {
+
+  return(
+    <div className='app'>
+      <Menu />
+      <Routes >
+        <Route path="/" element={ <HackerNews /> } />
+        <Route path="/new" element={ <New /> } />
+        <Route path="/past" element={ <Past /> } />
+        <Route path="/comments" element={ <Comments /> } />
+        <Route path="/ask" element={ <Ask /> } />
+        <Route path="/show" element={ <Show /> } />
+        <Route path="/jobs" element={ <Jobs /> } />
+        <Route path="/submit" element={ <Submit /> } />
+        <Route path="/login" element={ <Login /> } />
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
+
